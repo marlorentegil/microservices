1767773ffc9ff83bce9ca97bec50308f5387ffde
Mar Lorente Gil <marlorentegil@gmail.com>
2026-04-09 09:18:39 +0200

    Módulo project-service creado

---
+++ projects-service/src/main/java/com/aula/microservices/projects/ProjectsServiceApplication.java
@@ -0,0 +1,11 @@
+package com.aula.microservices.projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectsServiceApplication.class, args);
+    }
+}
