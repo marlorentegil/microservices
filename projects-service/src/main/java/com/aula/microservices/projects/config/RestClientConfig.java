77e84d215aceb37b6d779310b1fb653e32db76e1
Sergio <sanvitero2000@gmail.com>
2026-04-09 10:13:23 +0200

    Mejora en tema seugirad de los puertos en project-sercvice

--- projects-service/src/main/java/com/aula/microservices/projects/config/RestClientConfig.java
+++ projects-service/src/main/java/com/aula/microservices/projects/config/RestClientConfig.java
@@ -1,5 +1,6 @@
 package com.aula.microservices.projects.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -8,9 +9,10 @@ import org.springframework.web.client.RestClient;
 public class RestClientConfig {
 
     @Bean
-    public RestClient usersRestClient() {
+    public RestClient usersRestClient(
+            @Value("${clients.users.base-url}") String usersBaseUrl) {
         return RestClient.builder()
-                .baseUrl("http://localhost:8081")
+                .baseUrl(usersBaseUrl)
                 .build();
     }
 }
